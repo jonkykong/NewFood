8bf76105085e56778f9c53871ef3b5141783981e
jonkykong <jpkent@gmail.com>
2018-07-24 11:06:24 -0700

    One more change to prevent webview from opening ad urls externally on initial load.

--- screens/DetailView.js
+++ screens/DetailView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { 
   WebView, 
-  View,
+  SafeAreaView,
   Text,
   ActivityIndicator
 } from 'react-native';
@@ -16,21 +16,15 @@ export default class DetailView extends React.PureComponent {
     };
   }
 
-  _onLoadStart = (event) => {
-    const { didLoadEnd } = this.state;
-    const { url } = this.props;
-
-    if (didLoadEnd) {
-      this.webview.stopLoading();
-      this.props.onPress(url);
-    }
-  }
-
   _onShouldStartLoadWithRequest = (event) => {
-    const { didLoadEnd } = this.props;
+    const { didLoadEnd } = this.state;
+    
+    if (didLoadEnd) {
+      this.props.onPress(event.url);
+      return false;
+    }
 
-    this.props.url = event.url;
-    return !didLoadEnd;
+    return true;
   }
 
   render() {
@@ -39,17 +33,15 @@ export default class DetailView extends React.PureComponent {
     return (
       <WebView 
         style={{backgroundColor: Style.colors.clearBackgroundColor}}
-        ref={(ref) => this.webview = ref }
         source={{uri: url}} 
         onShouldStartLoadWithRequest={this._onShouldStartLoadWithRequest}
-        onLoadEnd={() => this.setState({ didLoadEnd: true }) }
-        onLoadStart={(event) => this._onLoadStart(event) }
+        onLoadEnd={() => setTimeout(() => this.setState({ didLoadEnd: true }), 250) } // Hack: webview seems to inconsistently post load events out of order to accurately know when to start opening urls externally.
         startInLoadingState={true}
         renderLoading={ () => {
             return (
-              <View style={Style.containerCenterXY}>
+              <SafeAreaView style={Style.containerCenterXY}>
                 <Text style={Style.textTitle}><ActivityIndicator />{' Hang on...'}</Text>
-              </View>
+              </SafeAreaView>
             );
           }
         }
